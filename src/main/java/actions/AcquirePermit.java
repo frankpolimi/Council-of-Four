9fc91c50f5c4585cc51d9596190332eb2758117a
Vitaliy Pakholko <2Pakes@gmail.com>
2016-05-04 21:32:07 +0200

    K

--- src/main/java/actions/AcquirePermit.java
+++ src/main/java/actions/AcquirePermit.java
@@ -16,42 +16,4 @@ public class AcquirePermit extends MainAction
 {
 
 
-	/*public void takeAction(Player player, PermitsDeck permitsDeck, BuildingPermit buildingPermit, ArrayList<PoliticsCard> politics)
-	{
-		if(permitsDeck.getFaceUpPermits().contains(buildingPermit))
-		{
-			int jollies=0;
-			Council council=new RegionalCouncil();
-			council.setCouncillors(permitsDeck.getCouncil().getCouncillors()); //Oppure Council council=(Council)permitsDeck.getCouncil().clone(); ma e' brutto
-			for(PoliticsCard card:politics) //Tra l'altro sta roba non posso manco farla
-			{
-				if(card instanceof JollyPoliticsCard)
-				{
-					jollies++;
-					continue;
-				}
-				
-				for(Councillor councillor:council.getCouncillors())
-				{
-					if(card.payCouncillor(councillor))
-					{
-						council.getCouncillors().remove(councillor);
-						continue;
-					}
-				}
-			}
-			
-			int diff=council.getCouncillors().size()-jollies;
-			int coins=player.getStatus().getCoins();
-			switch (diff)
-			{
-				case 0:return;
-				case 1: if(coins>=4)player.getStatus().setCoins(player.getStatus().getCoins()-4); break; //Manca tutta la parte di gestione del permesso che deve essere preso
-				case 2: if(coins>=7)player.getStatus().setCoins(player.getStatus().getCoins()-7); break; // e tolto dal mazzetto ed aggiunto il prossimo
-				case 3: if(coins>=10)player.getStatus().setCoins(player.getStatus().getCoins()-10); break;
-			}
-			
-				
-		}
-	}*/
 }
