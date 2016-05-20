ca004ec2468146d3eaea8ca6f99efbba7b69689b
Emanuele Ricciardelli <emanuele.ricciardelli@mail.polimi.it>
2016-05-20 09:42:58 +0200

    path file changed

--- src/main/java/cg2/game/MapMaker.java
+++ src/main/java/cg2/game/MapMaker.java
@@ -353,7 +353,7 @@ public class MapMaker {
 		//my choise: the XML file pathname is imposed by me to don't improve any errors with the 
 		//file opening. The map is always avaliable at that pathname and it is not allowed to change it.
 		SAXBuilder builder=new SAXBuilder();
-		Document document= builder.build(new File("src/main/map.xml"));
+		Document document= builder.build(new File("src/main/resources/map.xml"));
 		return document.getRootElement();
 	}
 	
