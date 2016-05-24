e25f2062a860b065b98974a91ea485fd3b25dd18
Vitaliy <vitaliy.pakholko@gmail.com>
2016-05-24 15:55:08 +0200

    Still parsers

---
+++ src/main/java/parsers/PermitsDeckParser.java
@@ -0,0 +1,16 @@
+package parsers;
+
+import cg2.model.PermitsDeck;
+
+public class PermitsDeckParser extends Parser
+{
+	public PermitsDeck parsePermitsDeck(String token)
+	{
+		if(token.startsWith("pd"))
+		{
+			n=Integer.parseInt(token.substring(2));
+			return model.getRegion().get(n-1).getPermitsDeck();
+		}
+		throw new IllegalArgumentException("Not valid permits deck parser command");
+	}
+}
