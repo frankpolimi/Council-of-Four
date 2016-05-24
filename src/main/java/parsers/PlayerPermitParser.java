b1e6f108a8bf146632eb6a7bf1b10e7458d61a80
Vitaliy <vitaliy.pakholko@gmail.com>
2016-05-24 12:25:36 +0200

    WIP Parsers

---
+++ src/main/java/parsers/PlayerPermitParser.java
@@ -0,0 +1,18 @@
+package parsers;
+
+import cg2.model.BuildingPermit;
+
+public class PlayerPermitParser extends Parser
+{
+	public BuildingPermit parsePlayerPermit(String token)
+	{
+		int n;
+		if(token.startsWith("pe"))
+		{
+			n=Integer.parseInt(token.substring(2));
+			return model.getPlayerPermit(0/*model.getCurrentPlayer*/).get(n-1);
+		}
+		throw new IllegalArgumentException("Not valid player permit parser command");
+	}
+
+}
