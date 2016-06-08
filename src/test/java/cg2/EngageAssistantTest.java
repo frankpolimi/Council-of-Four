28a648b630339f5bf1cccbed02174749fe3bf860
Vitaliy Pakholko <2Pakes@gmail.com>
2016-06-08 21:26:52 +0200

    Added forgotten "you already used all quick actions" test in elect councillor by assistant

--- src/test/java/cg2/EngageAssistantTest.java
+++ src/test/java/cg2/EngageAssistantTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 import java.io.IOException;
 import org.jdom2.JDOMException;
 import org.junit.Test;
+
+import model.actions.Action;
 import model.actions.EngageAssistant;
 import model.game.Game;
 
@@ -19,9 +21,10 @@ public class EngageAssistantTest
 			int CurrentPlayersCurrentAssistants=game.getCurrentPlayer().getAssistants();
 			int OtherPlayerCurrentAssistants=game.getPlayers().get(1).getAssistants();
 			game.getCurrentPlayer().setCoins(1);
+			Action a=new EngageAssistant();
 			try
 			{
-				new EngageAssistant().takeAction(game);
+				a.takeAction(game);
 			}
 			catch(IllegalStateException e)
 			{
@@ -30,7 +33,7 @@ public class EngageAssistantTest
 			game.getCurrentPlayer().setCoins(3);
 			try
 			{
-				new EngageAssistant().takeAction(game);
+				a.takeAction(game);
 			}
 			catch(IllegalStateException e)
 			{
@@ -40,7 +43,14 @@ public class EngageAssistantTest
 			assertEquals(CurrentPlayersCurrentAssistants+1, game.getCurrentPlayer().getAssistants());
 			assertEquals(OtherPlayerCurrentAssistants, game.getPlayers().get(1).getAssistants());
 			
-			
+			try
+			{
+				a.takeAction(game);
+			}
+			catch(IllegalStateException e)
+			{
+				assertTrue(true);
+			}
 			
 		} catch (JDOMException | IOException e) {
 			fail("Errors on game's creation");
