0fc3d7c841ef0bc8d9ef7189e71e7429038c9c7f
Emanuele Ricciardelli <emanuele.ricciardelli@mail.polimi.it>
2016-06-10 02:43:56 +0200

    viewProva aggiornata e migliorata.. ora pronta a testare tutto.. buonanotte

--- src/main/java/view/ViewProva.java
+++ src/main/java/view/ViewProva.java
@@ -10,11 +10,16 @@ import java.util.concurrent.ArrayBlockingQueue;
 
 import org.jdom2.JDOMException;
 
+import controller.BonusChange;
 import controller.Change;
 import controller.Controller;
+import controller.ModelChange;
+import controller.PermitsChange;
 import model.actions.AcquirePermit;
 import model.actions.Action;
+import model.actions.BuildEmporiumByPermit;
 import model.bonus.Bonus;
+import model.bonus.GetPoliticBonus;
 import model.bonus.NobilityBonus;
 import model.game.BuildingPermit;
 import model.game.Game;
@@ -59,6 +64,15 @@ public class ViewProva extends View {
 	public void update(Change change){
 		System.out.println("CAMBIAMENTO");
 		System.out.println(change);
+		if(change.getClass().equals(PermitsChange.class)){
+			PermitsChange permit=(PermitsChange)change;
+			System.out.println("Lista permessi"+permit.getPermits());
+		}else if(change.getClass().equals(BonusChange.class)){
+			BonusChange bonus=(BonusChange)change;
+			System.out.println("lista bonus "+bonus.getBonusList());
+		}else if(change.getClass().equals(ModelChange.class)){
+			System.out.println(((ModelChange)change).getGame().getCurrentPlayer());
+		}
 	}
 
 
@@ -74,8 +88,16 @@ public class ViewProva extends View {
 		players.add(p2);
 		players.add(p3);
 		game.setPlayers(players);
-		p1.setNobilityPoints(9);
+		p1.setNobilityPoints(15);
 		p1.setCoins(100000);
+		view.provaFinePartita(p1, view);
+
+	}
+	
+	public void avanzamentoNobiltà(Player p1, ViewProva view) throws InterruptedException{
+		game.getMap().getVertexByKey("K").addEmporium(p1);
+		game.getMap().getVertexByKey("j").addEmporium(p1);
+		game.getMap().getVertexByKey("A").addEmporium(p1);
 		Region mountain=game.getRegions().stream().filter(e->e.getName().equalsIgnoreCase("mountain")).findFirst().get();
 		List<ColoredPoliticsCard> carte=new ArrayList<>();
 		for(PoliticsCard pc:p1.getCardsOwned()){
@@ -111,9 +133,25 @@ public class ViewProva extends View {
 		carte3.addAll(carte2);
 		
 		game.setCurrentPlayer(p1);
-		
+		System.out.println("Carte usate "+carte3+"\n");
+		System.out.println("Carta prossima "+game.getPoliticsDeck().getCardAtIndex(0));
 		view.notifica(new ActionRequest((Action)new AcquirePermit(mountain.getCouncil(), carte3, permessoScelto),1));
+
+
+	}
+	
+	public void provaFinePartita(Player p1, ViewProva view){
+		p1.setRemainingEmporiums(1);
+		Set<City> cities=new HashSet<>();
+		cities.add(game.getMap().getVertexByKey("a"));
+		cities.add(game.getMap().getVertexByKey("c"));
+		Set<Bonus> bonuses=new HashSet<>();
+		bonuses.add(new	GetPoliticBonus(2));
 		
+		BuildingPermit permit=new BuildingPermit(cities, bonuses);
+		p1.getBuildingPermits().add(permit);
+		Request request=new ActionRequest(new BuildEmporiumByPermit(permit, game.getMap().getVertexByKey("C")),1);
+		view.notifica(request);
 	}
 
 }
