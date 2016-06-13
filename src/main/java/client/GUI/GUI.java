e3f183753d692de2c238936a78e1d375b918aee1
Emanuele Ricciardelli <emanuele.ricciardelli@mail.polimi.it>
2016-06-13 11:01:38 +0200

    mapUpdated

--- src/main/java/client/GUI/GUI.java
+++ src/main/java/client/GUI/GUI.java
@@ -14,10 +14,11 @@ import javax.swing.*;
 import java.awt.GridLayout;
 import javax.swing.GroupLayout.Alignment;
 import java.awt.FlowLayout;
+import java.awt.Dialog.ModalExclusionType;
 
 public class GUI extends JFrame {
 
-	private JPanel contentPane;
+	private JPanel completeMap;
 	private final String pathLand="src/main/resources/Immagini/mareA.jpg";
 	private final String pathHil="src/main/resources/Immagini/collinaA.jpg";
 	private final String pathMountain="src/main/resources/Immagini/montagnaA.jpg";
@@ -46,34 +47,42 @@ public class GUI extends JFrame {
 	public GUI() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 925, 650);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.Y_AXIS));
+		completeMap = new JPanel();
+		completeMap.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(completeMap);
+		completeMap.setLayout(null);
 		
 		JPanel map = new JPanel();
-		contentPane.add(map);
-		map.setLayout(new BoxLayout(map, BoxLayout.X_AXIS));
+		map.setBounds(0, 0, 900, 400);
+		completeMap.add(map);
+		map.setLayout(null);
 		ImagePanel land=new ImagePanel(pathLand);
+		land.setBounds(0, 0, 300, 400);
 		land.setAutoscrolls(true);
 		//JLabel land = new JLabel("REGIONE");
 		map.add(land);
 	
 		JPanel hill=new ImagePanel(pathHil);
+		hill.setBounds(300, 0, 300, 400);
 		map.add(hill);
 		
 		JPanel mountain=new ImagePanel(pathMountain);
+		mountain.setBounds(600, 0, 300, 400);
 		map.add(mountain);
 		
 		JPanel nobility = new JPanel();
-		contentPane.add(nobility);
-		nobility.setLayout(new BoxLayout(nobility, BoxLayout.X_AXIS));
+		nobility.setSize(900, 250);
+		nobility.setLocation(0, 400);
+		completeMap.add(nobility);
+		nobility.setLayout(null);
 		
 		JPanel nobility1 =new ImagePanelNobility(pathNobility1);
+		nobility1.setBounds(0, 0, 450, 250);
 		nobility.add(nobility1);
 		nobility1.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
 		
 		JPanel nobility2 =new ImagePanelNobility(pathNobility2);
+		nobility2.setBounds(450, 0, 450, 250);
 		nobility.add(nobility2);
 		nobility2.setLayout(new FlowLayout(FlowLayout.RIGHT, 5, 5));
 		
