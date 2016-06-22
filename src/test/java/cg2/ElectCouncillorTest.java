bfaf900eb089d23fe4de05ebd10c6f0e10a18c3b
Vitaliy Pakholko <2Pakes@gmail.com>
2016-06-22 13:45:44 +0200

    Updating Tests

--- src/test/java/cg2/ElectCouncillorTest.java
+++ src/test/java/cg2/ElectCouncillorTest.java
@@ -20,6 +20,7 @@ public class ElectCouncillorTest
 	Councillor validCouncillor;
 	Councillor notValidCouncillor;
 	Action action;
+
 	
 	public void electCouncillorSetup()
 	{
@@ -33,6 +34,8 @@ public class ElectCouncillorTest
 				notValidCouncillor=new Councillor(SupportClass.giveRandomColor());
 			}while(game.getAvaliableCouncillor().contains(notValidCouncillor));
 			
+			game.getAvaliableCouncillor().clear();
+			game.getAvaliableCouncillor().add(validCouncillor);
 			
 		} catch (JDOMException | IOException e) {
 			e.printStackTrace();
@@ -40,64 +43,63 @@ public class ElectCouncillorTest
 	}
 	
 	@Test
-	public void testTakeAction()
+	public void testActionWithANotValidCouncillorAndValidCouncil()
 	{
-			
-			ArrayList<Councillor> oldCouncil=new ArrayList<>();
-			for(Councillor co:validCouncil.getCouncillors())
-			{
-				oldCouncil.add(co);
-			}
-			game.getAvaliableCouncillor().clear();
-			game.getAvaliableCouncillor().add(validCouncillor);
-			
-			int oldPlayerCoins=game.getCurrentPlayer().getCoins();
-			/*for(Councillor co:validCouncil.getCouncillors())
-			{
-				oldCouncil.add(co);
-			}	*/	
-			action=new ElectCouncillor(notValidCouncillor, validCouncil);
-			try
-			{
-				action.takeAction(game);
-				fail("Action should launch exception");
-			}
-			catch(IllegalArgumentException e)
-			{
-				assertTrue(true);
-			}
-			
-			action=new ElectCouncillor(validCouncillor, notValidCouncil);
-			try
-			{
-				action.takeAction(game);
-				fail("Action should launch exception");
-			}
-			catch(IllegalArgumentException e)
-			{
-				assertTrue(true);
-			}
-			
-			action=new ElectCouncillor(validCouncillor, validCouncil);
-			assertTrue(action.takeAction(game));
-			
-			ArrayList<Councillor> newCouncil=new ArrayList<>();
-			for(Councillor co:validCouncil.getCouncillors())
-			{
-				newCouncil.add(co);
-			}
-			assertTrue(newCouncil.contains(validCouncillor));
-			for(int i=1;i<=3;i++)
-			{
-				assertTrue(newCouncil.contains(oldCouncil.get(i)));
-			}
-			assertTrue(game.getAvaliableCouncillor().size()==1);
-			assertTrue(game.getAvaliableCouncillor().get(0).equals(oldCouncil.get(0)));
-			assertTrue(newCouncil.contains(validCouncillor));
-			assertEquals(oldPlayerCoins+4, game.getCurrentPlayer().getCoins());
-			
-		
+		this.electCouncillorSetup();
+		action=new ElectCouncillor(notValidCouncillor, validCouncil);
+		try
+		{
+			action.takeAction(game);
+			fail("Action should launch exception");
+		}
+		catch(IllegalArgumentException e)
+		{
+			assertTrue(true);
+		}
 	}
 	
-
+	@Test
+	public void testActionWithValidCouncillorButNotValidCouncil()
+	{
+		this.electCouncillorSetup();
+		action=new ElectCouncillor(validCouncillor, notValidCouncil);
+		try
+		{
+			action.takeAction(game);
+			fail("Action should launch exception");
+		}
+		catch(IllegalArgumentException e)
+		{
+			assertTrue(true);
+		}
+	}
+	
+	@Test
+	public void testActionWithValidParameters()
+	{
+		this.electCouncillorSetup();
+		ArrayList<Councillor> oldCouncil=new ArrayList<>();
+		int oldPlayerCoins=game.getCurrentPlayer().getCoins();
+		for(Councillor co:validCouncil.getCouncillors())
+		{
+			oldCouncil.add(co);
+		}
+		action=new ElectCouncillor(validCouncillor, validCouncil);
+		assertTrue(action.takeAction(game));
+		
+		ArrayList<Councillor> newCouncil=new ArrayList<>();
+		for(Councillor co:validCouncil.getCouncillors())
+		{
+			newCouncil.add(co);
+		}
+		assertTrue(newCouncil.contains(validCouncillor));
+		for(int i=1;i<=3;i++)
+		{
+			assertTrue(newCouncil.contains(oldCouncil.get(i)));
+		}
+		assertTrue(game.getAvaliableCouncillor().size()==1);
+		assertTrue(game.getAvaliableCouncillor().get(0).equals(oldCouncil.get(0)));
+		assertTrue(newCouncil.contains(validCouncillor));
+		assertEquals(oldPlayerCoins+4, game.getCurrentPlayer().getCoins());
+	}
 }
