83cc1d9f10e6de8491f38f56901e76c6f0c4399b
Emanuele Ricciardelli <emanuele.ricciardelli@mail.polimi.it>
2016-06-25 15:55:24 +0200

    positionInTheMAP updated and XMLReaderForClient class finally finished

--- src/main/java/client/XMLReaderForClient.java
+++ src/main/java/client/XMLReaderForClient.java
@@ -26,7 +26,6 @@ public class XMLReaderForClient {
 	private static final String PATH="src/main/resources/positionInTheMap.xml";
 	
 	
-	private static final Dimension cityDim=new Dimension(GUI.singleRegionDimension.width*267/1000, GUI.singleRegionDimension.height*230/1000);
 	private static final Dimension bonusDim=new Dimension(GUI.singleRegionDimension.width*1282/10000, GUI.singleRegionDimension.width*1282/10000);
 	private static Dimension kingDim=bonusDim;
 	
@@ -39,7 +38,6 @@ public class XMLReaderForClient {
 	public void createCitiesFromRegionPanel(JPanel panel, Map<Character, City> bonuses, Dimension boardDim, Set<Region> regionSet) throws JDOMException, IOException{
 		List<Element> regions=this.getRootFromFile(PATH).getChild("cityCoordinates").getChildren();
 		Iterator<Element> regIt=regions.iterator();
-		int i=0;
 		
 		while(regIt.hasNext()){
 			Element region=regIt.next();
@@ -66,25 +64,15 @@ public class XMLReaderForClient {
 			Iterator<Element> cityIt=cities.iterator();
 			while(cityIt.hasNext()){
 				Element cityElement=cityIt.next();
-				
-				//Dimension cityDim=new Dimension(Integer.parseInt(cityElement.getAttributeValue("width")),Integer.parseInt(cityElement.getAttributeValue("height")));
-				//Dimension bonusDim=new Dimension((int)(((double)45/(double)cityDim.width)*((double)cityDim.width/(double)280)*regionPanel.getWidth()),(int)(((double)45/(double)cityDim.width)*((double)cityDim.width/(double)280)*regionPanel.getWidth()));
-				//Dimension kingDim=new Dimension((int)(((double)45/(double)cityDim.width)*((double)cityDim.width/(double)280)*regionPanel.getWidth()),(int)(((double)45/(double)cityDim.width)*((double)cityDim.width/(double)280)*regionPanel.getWidth()));
+				Dimension cityDim=new Dimension(GUI.singleRegionDimension.width*Integer.parseInt(cityElement.getAttributeValue("width"))/1000, GUI.singleRegionDimension.height*Integer.parseInt(cityElement.getAttributeValue("height"))/1000);
 				JPanel newPanel=new JPanel();
 				newPanel.setName(cityElement.getAttributeValue("name"));
 				newPanel.setLayout(null);
 				newPanel.setOpaque(false);
-				//newPanel.setBounds((int)((Double.parseDouble(cityElement.getAttributeValue("x"))/(double)840)*boardDim.width)-i*regionPanel.getWidth(), (int)((Double.parseDouble(cityElement.getAttributeValue("y"))/(double)768)*boardDim.height), 
-				//		cityDim.width,cityDim.height);
 				newPanel.setLocation(Integer.parseInt(cityElement.getAttributeValue("xRel"))*GUI.singleRegionDimension.width/1000, 
 						Integer.parseInt(cityElement.getAttributeValue("yRel"))*GUI.singleRegionDimension.height/1000);
 				newPanel.setSize(cityDim);
-				newPanel.setBorder(new LineBorder(Color.black));
 				City city=bonuses.get(cityElement.getAttributeValue("name").charAt(0));
-				/*JPanel kingPanel=new JPanel();
-				kingPanel.setName("kingPanel");
-				kingPanel.setBounds((newPanel.getWidth()/2)-(kingDim.width/2), (newPanel.getHeight()/2)-(kingDim.height/2), kingDim.width, kingDim.height);
-				newPanel.add(kingPanel);*/
 				JPanel kingPanel;
 				if(!city.getBonusImagePath().isEmpty()){
 					JPanel bonus=new ImagePanel(bonuses.get(cityElement.getAttributeValue("name").charAt(0)).getBonusImagePath(), bonusDim);
@@ -101,14 +89,11 @@ public class XMLReaderForClient {
 				kingPanel.setName("kingPanel");
 				kingPanel.setOpaque(false);
 				kingPanel.setBounds((newPanel.getWidth()/2)-(kingDim.width/2),(newPanel.getHeight()/2)-(kingDim.height/2), kingDim.width, kingDim.height);
-				//kingPanel.setLocation(0, bonusDim.height);
 				kingPanel.setSize(bonusDim);
-				kingPanel.setBorder(new LineBorder(Color.red,4));
 				newPanel.add(kingPanel);
 					
 				regionPanel.add(newPanel);
 			}
-			i++;
 		}
 	}
 }
